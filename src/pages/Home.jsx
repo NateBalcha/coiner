4ebd87d3c0d6818ad97612e7238275e0e5fe790f
NateBalcha <natebalcha@gmail.com>
2023-12-14 14:47:43 -0500

    sign up fixed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -39,28 +39,7 @@ export default function Home() {
           </div>
         </div>
         {/* sign-up */}
-        <div className="signup">
-          <div className="container">
-            {/* left */}
-            <div className="left">
-              <img src={Crypto} alt="" />
-            </div>
 
-            {/* right */}
-            <div className="right">
-              <h2>Earn passive income with crypto.</h2>
-              <p>
-                Earn up to 12% annual rewards on 30+ digital assets. Simply hold
-                your assets in the app to automatically earn rewards at the end
-                of each month with no lockups and no limits.
-              </p>
-              <div className="input-container">
-                <input type="email" placeholder="Enter your email" />
-                <button className="btn">Learn More</button>
-              </div>
-            </div>
-          </div>
-        </div>
         <div className="width">
           <h2>Search for a coin</h2>
 
@@ -106,6 +85,28 @@ export default function Home() {
               {store.coins.map((coin) => {
                 return <ListItem key={coin.id} coin={coin} />;
               })}
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="signup">
+        <div className="container">
+          {/* left */}
+          <div className="left">
+            <img src={Crypto} alt="" />
+          </div>
+
+          {/* right */}
+          <div className="right">
+            <h2>Earn passive income with crypto.</h2>
+            <p>
+              Earn up to 12% annual rewards on 30+ digital assets. Simply hold
+              your assets in the app to automatically earn rewards at the end of
+              each month with no lockups and no limits.
+            </p>
+            <div className="input-container">
+              <input type="email" placeholder="Enter your email" />
+              <button className="btn">Learn More</button>
             </div>
           </div>
         </div>
